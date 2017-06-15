ef831fd4f3ff1459242fb2cd89b7548259c5861c
STNIN <tyhayaci@gmail.com>
2017-06-15 13:06:21 -0300

    Delete digitControl2.h

--- digitControl2.h
+++
@@ -1,8 +0,0 @@
-#ifndef digitControl2_H_INCLUDED
-#define digitControl2_H_INCLUDED
-
-int digito_controle_2 (int cpf_int []);
-int i, multiplicar2 = 11, soma2 = 0, teste2 = 0;
-int controle2 = 0;
-
-#endif
